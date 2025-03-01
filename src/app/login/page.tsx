44c97e755cd7ad8bf40e114f8dd615824f1ed30f
ThisIsTheNewOne <97050679+ThisIsTheNewOne@users.noreply.github.com>
2025-03-01 13:56:51 +0100

    adding login-widget style

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,19 @@
 import styles from "./page.module.css";
+import { roboto } from "@/app/ui/fonts";
 
 export default function Page() {
 
   return (
     <div className={`${styles.page} ${styles.heroBackground}`}>
       <main className={styles.main}>
+        <div className={styles.loginBox}>
+          <div className={styles.innerContainer}>
+            <input type="text" placeholder="Username" className={`${roboto.className} ${styles.input}  ${styles.bold}`} />
+            <input type="password" placeholder="Password" className={`${roboto.className} ${styles.input}  ${styles.bold}`} />
+            <button className={`${styles.button} ${styles.normal}`}>Sign In</button>
+          </div>
+        </div>
       </main>
-      <footer className={styles.footer}>
-      </footer>
     </div>
   );
 }
