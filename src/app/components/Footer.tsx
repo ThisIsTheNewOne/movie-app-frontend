d787b4a599e5c33ecf39464acfa2dfb32f5c56d2
ThisIsTheNewOne <97050679+ThisIsTheNewOne@users.noreply.github.com>
2025-03-01 22:45:06 +0100

    adding Footer dummy text + style

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,29 @@
+import styles from '../page.module.css'
+
 export default function Footer() {
-
-
-    return (
-        <div>Footer Page</div>
-    );
-  } 
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerContent}>
+        <div className={styles.column}>
+          <a href="#">About Us</a>
+          <a href="#">Contact</a>
+          <a href="#">Blog</a>
+          <a href="#">FAQ</a>
+        </div>
+        
+        <div className={styles.column}>
+          <a href="#">Terms of Service</a>
+          <a href="#">Privacy Policy</a>
+        </div>
+        
+        <div className={styles.column}>
+          <a href="#">Subscribe</a>
+        </div>
+        
+        <div className={styles.column}>
+          <a href="#">Social Media</a>
+        </div>
+      </div>
+    </footer>
+  );
+}
