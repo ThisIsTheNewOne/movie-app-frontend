81cbdffc0c80af6ad68fd7e7e1de3e9476c7e7ea
ThisIsTheNewOne <97050679+ThisIsTheNewOne@users.noreply.github.com>
2025-03-01 20:19:11 +0100

    adding userList of movies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 "use client";
 
+import Image from 'next/image';
 import styles from "./page.module.css";
 // import { roboto } from "@/app/ui/fonts";
 import { redirect } from "next/navigation";
 import { useUser } from "./lib/UserContext";
 import { Movie } from "./lib/types";
 import Carousel from "./components/Carousel";
+import ScrollableMovies from "./components/ScrollableMovies";
 // import clsx from "clsx";
 
 import moviesList from "./lib/data/movies.json";
 import genresList from "./lib/data/genresList.json";
-import ScrollableMovies from "./components/ScrollableMovies";
+import userList from "./lib/data/userList.json";
 
 export default function Home() {
   const { user } = useUser();
@@ -27,6 +29,10 @@ export default function Home() {
     ...genre,
     movies: (moviesList as Movie[]).filter((movie) => movie.genre === genre.id),
   }));
+
+  const userListMovies = (moviesList as Movie[]).filter((movie) =>
+    userList.includes(movie.id)
+  );
 
   return (
     <div className={styles.page}>
@@ -48,6 +54,23 @@ export default function Home() {
                 <ScrollableMovies movies={genre.movies} />
               </section>
             ))}
+          </div>
+          <div>
+            <h2>My List</h2>
+            <div>
+              {userListMovies.map((movie) => (
+                <div key={movie.id} className={styles.movieCard}>
+                  <Image
+                    src={movie.thumbnail || "/fallback-image.jpg"}
+                    alt={movie.title}
+                    width={261}
+                    height={386}
+                    className={styles.movieImage}
+                    priority
+                  />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </main>
